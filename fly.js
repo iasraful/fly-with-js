0d4aa570758f271c52983678367b347dfbe81a7e
unknown <iasraful321@gmail>
2021-01-31 22:02:54 +0600

    fly with javaScript

--- fly.js
+++ fly.js
@@ -1,56 +1,55 @@
-document.getElementById('increase') .addEventListener('click', function name() {
-  productsHandeler(true)  
-})
 
-document.getElementById('decrease') .addEventListener('click', function () {
-   productsHandeler(false)
-})
-
-
-function productsHandeler(isIncrease) {
-    const ticketInput = document.getElementById('money-count')
-    const ticketCount = parseInt(ticketInput.value);
+function productsHandeler(ticket , isIncrease) {
+    let ticketInput = document.getElementById(ticket) // money
+    let ticketCount = parseInt(ticketInput.value);
     // const ticketNewCount = ticketCount- 1;
     let ticketNewCount = ticketCount;
     if (isIncrease == true) {
         ticketNewCount = ticketCount + 1;
-        
     }
-    if (isIncrease == false && ticketCount>0) {
+    if (isIncrease == false && ticketCount > 0) {
         ticketNewCount = ticketCount - 1;
-        
     }
     ticketInput.value = ticketNewCount;
-    const newTotal = ticketNewCount * 150;
+    let newTotal = 0;
+    if (ticket == "money") {
+        newTotal = ticketNewCount * 150;
+    }
+    if (ticket == "economy") {
+        newTotal = ticketNewCount * 100;
+    }
+
     document.getElementById('subTotal').innerText = '$'+ newTotal;
+
+    totalTicketPrice();
 }
 
-//  Economy Ticket count
-document.getElementById('economy-increase').addEventListener('click', function name() {
-    const ticketInput = document.getElementById('economy-money')
-    const ticketCount = parseInt(ticketInput.value);
-    const ticketNewCount = ticketCount+ 1;
-    ticketInput.value = ticketNewCount;
-    var economyNewTotal = ticketNewCount * 100;
-    document.getElementById('subTotal').innerText = '$'+ economyNewTotal;
+function totalTicketPrice(){
+    let moneyInput = dynamicInput('money');
+    let economynput = dynamicInput('economy');
+
+    let subTotal = moneyInput * 150 + economynput * 100;
+    document.getElementById('subTotal').innerText = '$' + subTotal;
+
+    let vat = Math.round(subTotal * 0.1);
+    document.getElementById('vat').innerText = '$' + vat;
+
+    let totalCost = subTotal + vat;
+    document.getElementById('totalCost').innerText = '$' + totalCost;
+}
+
+function dynamicInput(ticket){
+    let ticketInput = document.getElementById(ticket) // money
+    let ticketCount = parseInt(ticketInput.value);
+
+    return ticketCount;
+}
+
+
+var bookingButton = document.getElementById('booking');
+bookingButton.addEventListener('click', function () {
+    var bookingArea = document.getElementById('booking-btn');
+    bookingArea.style.display="none";
+    var paymentStatus = document.getElementById('payment-confermation');
+    paymentStatus.style.display = 'block';
 })
-
-
-// document.getElementById('increase') .addEventListener('click', function name() {
-//     const ticketInput = document.getElementById('money-count')
-//     const ticketCount = parseInt(ticketInput.value);
-//     const ticketNewCount = ticketCount+ 1;
-//     ticketInput.value = ticketNewCount;
-//     const newTotal = ticketNewCount * 150;
-//     document.getElementById('total')
-    
-// })
-
-// document.getElementById('decrease') .addEventListener('click', function () {
-//     const ticketInput = document.getElementById('money-count')
-//     const ticketCount = parseInt(ticketInput.value);
-//     const ticketNewCount = ticketCount- 1;
-//     ticketInput.value = ticketNewCount;
-//     const newTotal = ticketNewCount * 150;
-//     document.getElementById('total')
-// })
